695f949874b9d94ed1fc0e2dcacd24c14a300988
BBeerBear <xiongx659@gmail.com>
2024-02-26 23:30:49 -0500

    Initial commit

---
+++ src/main/java/com/bbeerbear/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.bbeerbear.ecommerce.dao;
+
+import com.bbeerbear.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
